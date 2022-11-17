dda0175d16cdcfe508919b648d275c2ebeda83c0
Equis <equis.xdb@gmail.com>
2022-11-17 20:40:37 -0300

    se sube ejercicio 3 java basico OB

---
+++ Ejercicio 3/Ejercicio 3/src/ob/ejericio3/Ejercicio3.java
@@ -0,0 +1,17 @@
+package ob.ejericio3;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Harry", "Hermione", "Ron", "Hagrid" };
+
+        for (String nombre : nombres) {
+
+            System.out.print(nombre + " ");
+
+        }
+
+    }
+
+}
