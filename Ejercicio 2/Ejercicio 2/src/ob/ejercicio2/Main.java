e21e6a1932b298e97fa214d1e75b38bbb8a73aa0
Equis <equis.xdb@gmail.com>
2022-11-17 19:19:28 -0300

    Se agrega imput del usuario

--- Ejercicio 2/Ejercicio 2/src/ob/ejercicio2/Main.java
+++ Ejercicio 2/Ejercicio 2/src/ob/ejercicio2/Main.java
@@ -1,10 +1,20 @@
 package ob.ejercicio2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        double precioConIva = calcularIva(222);
-        System.out.println(precioConIva);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce un precio: ");
+
+        int precio = scanner.nextInt();
+
+
+        double precioConIva = calcularIva(precio);
+        System.out.println("El precio con IVA es" + " " + precioConIva);
 
     }
 
