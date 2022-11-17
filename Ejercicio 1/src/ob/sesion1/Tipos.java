a79c7606c577023364ac5f75ffdfe8e4d32a5eb8
Equis <equis.xdb@gmail.com>
2022-11-17 17:38:10 -0300

    Se sube ejercicio 1 de java basico OB

---
+++ Ejercicio 1/src/ob/sesion1/Tipos.java
@@ -0,0 +1,37 @@
+package ob.sesion1;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+
+        //Imprime todos los tipos de datos numericos enteros
+        byte variable1 = 2;
+        System.out.println(variable1);
+        short variable2 = 9;
+        System.out.println(variable2);
+        int variable3 = 34;
+        System.out.println(variable3);
+        long variable4 = 344;
+        System.out.println(variable4);
+
+        //Imprime todos los tipos de datos numericos decimales
+        float variable5 = 2.2f;
+        System.out.println(variable5);
+        double variable6 = 2.222d;
+        System.out.println(variable6);
+
+        //imprime todos los tipos de datos booleanos
+        boolean variable7 = true;
+        System.out.println(variable7);
+        boolean variable8 = false;
+        System.out.println(variable8);
+
+        //Imprime todos los tipos de datos de texto
+        char variable9 = 'a';
+        System.out.println(variable9);
+        String variable10 = "Esto es un String";
+        System.out.println(variable10);
+        
+
+    }
+}
